581ad6b20e3809440dbb9bceebde727a5ab0ae55
Scott Macfarlane <scott@gina.alaska.edu>
2016-03-09 16:06:13 -0900

    Add chef-sugar

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,6 +3,7 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 UAF-GINA Rights Reserved.
+include_recipe 'chef-sugar'
 
 include_recipe 'resolver::default'
 include_recipe 'ntp::default'
@@ -10,6 +11,6 @@ include_recipe 'chef-client::config'
 include_recipe 'chef-client::service'
 include_recipe 'selinux::permissive'
 include_recipe 'omnibus_updater'
-include_recipe 'iptables-ng::default'
+include_recipe 'iptables-ng::default' unless docker?
 include_recipe 'sandy-base::ssh'
 include_recipe 'sandy-base::users'
--- recipes/ssh.rb
+++ recipes/ssh.rb
@@ -1,9 +1,12 @@
 include_recipe 'openssh::default'
-Chef::Resource::IptablesNgRule.send(:include, SandyBase::Helper)
 
-iptables_ng_rule '22-ssh-enable-gina-rcs-hosts' do
-  ip_version 4
-  rule "-m state --state NEW -p tcp -s #{ssh_allowed_networks} --dport 22 -j ACCEPT"
+unless docker?
+  Chef::Resource::IptablesNgRule.send(:include, SandyBase::Helper)
+
+  iptables_ng_rule '22-ssh-enable-gina-rcs-hosts' do
+    ip_version 4
+    rule "-m state --state NEW -p tcp -s #{ssh_allowed_networks} --dport 22 -j ACCEPT"
+  end
 end
 
 include_recipe "ssh_known_hosts::default"
